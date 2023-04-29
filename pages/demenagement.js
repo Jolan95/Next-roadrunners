0ecfcebf3cf0e39ad1070b3e48d5c5f98b61d0b7
Jolan95 <jolan.aubry@hotmail.fr>
2023-04-29 13:17:56 +0200

    demenagement pic

--- pages/demenagement.js
+++ pages/demenagement.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/header'
-import Demenageurs from "../assets/demenageurs.webp"
+import Demenageurs from "../assets/demenagement.jpg"
 import Footer from '../components/footer'
 import Carton from "../assets/demenagement-carton.webp"
 import Link from 'next/link'
