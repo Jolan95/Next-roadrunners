f69a35ede6cb92f4c8da6dba8d03b4e777ed20df
Jolan95 <jolan.aubry@hotmail.fr>
2023-04-29 14:40:23 +0200

    van image

--- components/first-section.js
+++ components/first-section.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function FirstSection(props) {
   return (
     <section className='section-presentation' style={{backgroundImage: `url(`+props.bg.src+`)`, }}>
-        <div className='h-full flex items-end justify-center'>
+        <div className='h-full flex items-end justify-start'>
             <div className="pl-4 pb-6">
-                <h1 className='md:text-6xl text-2xl   kanit text-white'>{props.title}</h1>
+                <h1 className='md:text-6xl text-2xl kanit text-white'>{props.title}</h1>
                 <h2 className='md:text-2xl text-xl font-semibold  text-white'></h2>
             </div>
         </div>
