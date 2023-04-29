22050bd827591f6a951ca7d52391370fa23b6695
Jolan95 <jolan.aubry@hotmail.fr>
2023-04-29 14:30:07 +0200

    van image

--- components/first-section.js
+++ components/first-section.js
@@ -5,7 +5,7 @@ export default function FirstSection(props) {
     <section className='section-presentation' style={{backgroundImage: `url(`+props.bg.src+`)`, }}>
         <div className='h-full flex items-end justify-center'>
             <div className="pl-4 pb-6">
-                <h1 className='md:text-7xl text-2xl  color-logo kanit'>{props.title}</h1>
+                <h1 className='md:text-6xl text-2xl  color-logo kanit'>{props.title}</h1>
                 <h2 className='md:text-2xl text-xl font-semibold  text-white'></h2>
             </div>
         </div>
